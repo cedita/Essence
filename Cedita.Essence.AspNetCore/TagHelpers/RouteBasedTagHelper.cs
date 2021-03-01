1c54e2ebb4f6920bff8b0bd94c3bd682306be868
Rudi Visser <me@rudi.dev>
2021-03-01 20:28:47 +0000

    AspNetCore 1.0.3 - Better matching extension

--- Cedita.Essence.AspNetCore/TagHelpers/RouteBasedTagHelper.cs
+++ Cedita.Essence.AspNetCore/TagHelpers/RouteBasedTagHelper.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0.
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
@@ -17,7 +18,6 @@ namespace Cedita.Essence.AspNetCore.TagHelpers
         private readonly EssenceTagHelperOptions options;
 
         protected RouteBasedTagHelper(IHttpContextAccessor httpContextAccessor, ILoggerFactory loggerFactory, IOptions<EssenceTagHelperOptions> options)
-            : base()
         {
             this.httpContextAccessor = httpContextAccessor;
             this.logger = loggerFactory.CreateLogger<RouteBasedTagHelper>();
@@ -59,8 +59,59 @@ namespace Cedita.Essence.AspNetCore.TagHelpers
                     }
                 }
 
-                return matchVals.Any(m => string.Equals(optionVal, m, StringComparison.InvariantCultureIgnoreCase));
+                if (!matchVals.Any(m => m.Contains("[")))
+                {
+                    return matchVals.Any(m => string.Equals(optionVal, m, StringComparison.InvariantCultureIgnoreCase));
+                }
+
+                // If we're checking further route values
+                var enhancedMatchValues = new List<EnhancedMatch>();
+                foreach (var matchVal in matchVals)
+                {
+                    if (!matchVal.Contains("["))
+                    {
+                        enhancedMatchValues.Add(new EnhancedMatch { BaseMatch = matchVal });
+                        continue;
+                    }
+
+                    int matchStart = matchVal.IndexOf("[", StringComparison.InvariantCulture) + 1,
+                        matchEnd = matchVal.LastIndexOf("]", StringComparison.InvariantCulture);
+                    var matchSplit = matchVal.Substring(matchStart, matchEnd - matchStart).Split('=');
+
+                    enhancedMatchValues.Add(new EnhancedMatch
+                    {
+                        BaseMatch = matchVal.Substring(0, matchStart - 1),
+                        QueryParamName = matchSplit[0].ToLowerInvariant(),
+                        QueryParamValue = matchSplit[1].ToLowerInvariant(),
+                    });
+                }
+
+                return enhancedMatchValues.Any(m =>
+                {
+                    var baseMatch = string.Equals(optionVal, m.BaseMatch, StringComparison.InvariantCultureIgnoreCase);
+                    if (!baseMatch)
+                    {
+                        return false;
+                    }
+
+                    if (m.QueryParamName == null)
+                    {
+                        return true;
+                    }
+
+                    var queryStringVal = httpContextAccessor.HttpContext.Request.Query[m.QueryParamName];
+                    return string.Equals(queryStringVal, m.QueryParamValue, StringComparison.InvariantCultureIgnoreCase);
+                });
             });
+        }
+
+        private class EnhancedMatch
+        {
+            public string BaseMatch { get; set; }
+
+            public string QueryParamName { get; set; }
+
+            public string QueryParamValue { get; set; }
         }
     }
 }
